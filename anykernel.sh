e4db3162fdc7f2f25424cd2084a2a22fad82d267
John Macka <macka2004@gmail.com>
2022-04-03 13:32:33 +1000

    Anykernel3: lmi-immensity kernel

--- anykernel.sh
+++ anykernel.sh
@@ -9,8 +9,8 @@ do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=raphael
-device.name2=raphaelin
+device.name1=lmi
+device.name2=lmipro
 
 supported.versions=
 supported.patchlevels=
@@ -20,85 +20,36 @@ supported.patchlevels=
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-
+patch_vbmeta_flag=auto;
+no_block_display=1
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-## begin vendor changes
-mount -o rw,remount -t auto /vendor >/dev/null;
+mv $home/kernels/$os/Image $home/Image;
+mv $home/kernels/$os/dtb $home/dtb;
+mv $home/kernels/$os/dtbo.img $home/dtbo.img;
 
-# Make a backup of init.target.rc
-restore_file /vendor/etc/init/hw/init.target.rc;
-
-## AnyKernel install
+## AnyKernel boot install
 split_boot;
-
-#
-#case "$ZIPFILE" in
-#  *oc*)
-#    ui_print "  • Setting 75 Hz refresh rate"
-#    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-#    ;;
-#  *)
-#    patch_cmdline "msm_drm.framerate_override" ""
-#    fr=$(cat /sdcard/framerate_override | tr -cd "[0-9]");
-#    [ $fr -eq oc ] && ui_print "  • Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-#    ;;
-#esac
-#
-
-if mountpoint -q /data; then
-  # Optimize F2FS extension list (@arter97)
-  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
-    hash="$(md5sum $list_path | sed 's/extenstion/extension/g' | cut -d' ' -f1)"
-
-    # Skip update if our list is already active
-    if [[ $hash == "43df40d20dcb96aa7e8af0e3d557d086" ]]; then
-      echo "Extension list up-to-date: $list_path"
-      continue
-    fi
-
-    ui_print "  • Optimizing F2FS extension list"
-    echo "Updating extension list: $list_path"
-
-    echo "Clearing extension list"
-
-    hot_count="$(grep -n 'hot file extens' $list_path | cut -d':' -f1)"
-    list_len="$(cat $list_path | wc -l)"
-    cold_count="$((list_len - hot_count))"
-
-    cold_list="$(head -n$((hot_count - 1)) $list_path | grep -v ':')"
-    hot_list="$(tail -n$cold_count $list_path)"
-
-    for ext in $cold_list; do
-      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
-    done
-
-    for ext in $hot_list; do
-      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
-    done
-
-    echo "Writing new extension list"
-
-    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
-      [ ! -z $ext ] && echo "[c]$ext" > $list_path
-    done
-
-    for ext in $(cat $home/f2fs-hot.list); do
-      [ ! -z $ext ] && echo "[h]$ext" > $list_path
-    done
-  done
-fi
 
 flash_boot;
 flash_dtbo;
 
-#ui_print "  • Adding IMMENS1TY Service Module";
-#rm -rf /data/adb/modules/IMMENS1TYmodule;
-#mkdir -p /data/adb/modules/IMMENS1TYmodule;
-#cp -rf IMMENS1TYmodule/ /data/adb/modules/;
+## end boot install
 
-## end install
+# Vendor boot
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
 
+# reset for vendor_boot patching
+#reset_ak;
+
+## AnyKernel vendor_boot install
+#split_boot;
+
+#flash_boot;
+## end vendor_boot install
